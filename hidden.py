de4650acf7e847c0982083ad760462c48d08aa06
Nayan Bagale <nvbagale@yahoo.com>
2020-04-15 19:12:58 +0530

    Delete hidden.py

--- hidden.py
+++
@@ -1,9 +0,0 @@
-def head():
-    headers = {
-        'x-rapidapi-host': "corona-virus-world-and-india-data.p.rapidapi.com",
-        'x-rapidapi-key': "__________________________________"
-        }
-    return headers
-
-if __name__ == "__main__":
-    print("Run the api.py file")
